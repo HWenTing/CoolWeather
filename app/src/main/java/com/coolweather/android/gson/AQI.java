5279dcb7cf4c2d3a81fb626adc08212cbd858f28
HWenTing <982740445@qq.com>
2020-12-27 20:20:16 +0800

    提交

---
+++ app/src/main/java/com/coolweather/android/gson/AQI.java
@@ -0,0 +1,13 @@
+package com.coolweather.android.gson;
+
+/**
+ * Created by HP on 2017/5/4.
+ */
+
+public class AQI {
+    public AQICity city;
+    public class AQICity{
+        public String aqi;
+        public String pm25;
+    }
+}
